4ce7dc6af3b1153d90128acb5ce29e5391b3933d
chadgarlandscg <chadgarlandscg@gmail.com>
2024-08-19 21:14:28 -0600

    Fix typo

--- iac/initialization/main.tf
+++ iac/initialization/main.tf
@@ -13,5 +13,5 @@ module "fargate_bounded_context_initialization" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/initialization?ref=feature/aws-lambda-boilerplate-deployment"
   bounded_context_name = "LambdaBoilerplate"
   build_id = var.build_id
-  source_code_path = "${path.module}../../out/index.js"
+  source_code_path = "${path.module}/../../out/index.js"
 }
