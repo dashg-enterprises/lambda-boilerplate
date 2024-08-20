773f5fd46330db035025f7f94a001f007e4272e1
chadgarlandscg <chadgarlandscg@gmail.com>
2024-08-19 21:37:53 -0600

    Copy zip to S3 from git action instead of TF

--- iac/initialization/main.tf
+++ iac/initialization/main.tf
@@ -12,6 +12,6 @@ terraform {
 module "fargate_bounded_context_initialization" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/initialization?ref=feature/aws-lambda-boilerplate-deployment"
   bounded_context_name = "LambdaBoilerplate"
-  build_id = var.build_id
-  source_code_path = "${path.module}/../../out/index.js"
+#   build_id = var.build_id
+#   source_code_path = "${path.module}/../../out/index.zip"
 }
