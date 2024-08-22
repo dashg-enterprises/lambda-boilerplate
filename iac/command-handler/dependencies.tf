0741887801b1bf8d7776a3f3e6fe97a611462168
chadgarlandscg <chadgarlandscg@gmail.com>
2024-08-21 23:42:07 -0600

    Move bounded context dependencies to the command handler

---
+++ iac/command-handler/dependencies.tf
@@ -0,0 +1,49 @@
+
+resource "aws_dynamodb_table" "aggregate_event_log" {
+  name         = "${var.application_name}EventLog"
+  billing_mode = "PAY_PER_REQUEST"
+  # read_capacity  = 10
+  # write_capacity = 5
+
+  hash_key = "aggregateId"
+  attribute {
+    name = "aggregateId"
+    type = "S" # String data type
+  }
+
+  attribute {
+    name = "sequenceId"
+    type = "S" # String data type
+  }
+
+  global_secondary_index {
+    name            = "sequence_id_index"
+    hash_key        = "sequenceId"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name = "${var.application_name}EventLog"
+  }
+}
+
+resource "aws_dynamodb_table" "aggregate_snapshots" {
+  name         = "${var.application_name}Snapshots"
+  billing_mode = "PAY_PER_REQUEST"
+  # read_capacity  = 10
+  # write_capacity = 5
+
+  hash_key = "id"
+  attribute {
+    name = "id"
+    type = "S" # String data type
+  }
+
+  tags = {
+    Name = "${var.aggregate_root_name}Snapshots"
+  }
+}
+
+resource "aws_cloudwatch_event_bus" "event_bridge_bus" {
+  name = "${var.aggregate_root_name}EventBus"
+}
