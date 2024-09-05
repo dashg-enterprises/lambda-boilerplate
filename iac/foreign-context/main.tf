0c2f581a0cbe164388b96050f3cfd214a2f22102
chadgarlandscg <chadgarlandscg@gmail.com>
2024-09-05 00:39:16 -0600

    Point to module main

--- iac/foreign-context/main.tf
+++ iac/foreign-context/main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 module "example_lambda_context" {
-  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=feature/lambda-bounded-context"
-  bounded_context_name = "DemoContext"
+  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=main"
+  bounded_context_name = "DemonstrationContext"
   aggregate_root_name = "Demonstration"
   application_name = "demonstration-handlers-${var.environment_name}"
   build_bucket_name = var.build_bucket_name
--- iac/main.tf
+++ iac/main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 module "example_lambda_context" {
-  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=feature/lambda-bounded-context"
-  bounded_context_name = "LambdaBoilerplate"
+  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=main"
+  bounded_context_name = "ExampleContext"
   aggregate_root_name = "Example"
   application_name = "example-handlers-${var.environment_name}"
   build_bucket_name = var.build_bucket_name
