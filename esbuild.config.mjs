a5afe25afc255bfcdc4a9691c033d5098a00aa43
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-11 23:27:48 -0700

    Enable debug build mode

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,14 +2,14 @@ import { build } from 'esbuild';
 import { esbuildDecorators } from 'esbuild-decorators'
 import { argv } from 'node:process';
 
-const environment = argv[2];
+const buildMode = argv[2];
 const lambdaEntryPoint = argv[3];
 
-if (environment !== 'local' && environment !== 'prod') throw new Error("Must select local or prod at argv[2]");
+if (buildMode !== 'debug' && buildMode !== 'release') throw new Error("Must select debug or release at argv[2]");
 if (!lambdaEntryPoint) throw new Error("No lambda entry point provided at argv[3]");
 
-const isLocal = environment === 'local';
-const isProd = environment === 'prod';
+const isDebug = buildMode === 'debug';
+const isRelease = buildMode === 'release';
 
 async function lambdaBuilder(
   tsconfig = "./tsconfig.json",
@@ -20,9 +20,9 @@ async function lambdaBuilder(
   await build({
     platform: 'node',
     target: 'es2020',
-    sourcemap: isLocal,
-    // minify: isProd,
-    bundle: isProd,
+    sourcemap: isDebug,
+    // minify: isRelease,
+    bundle: isRelease,
     plugins: [
       esbuildDecorators({
         tsconfig,
