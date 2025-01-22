ae1c6a0bc4d8c6876a43c71edfc55446f1c3bb39
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-22 01:14:45 -0700

    Output build bucket name

--- iac/initialization/main.tf
+++ iac/initialization/main.tf
@@ -9,7 +9,11 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-module "fargate_bounded_context_initialization" {
-  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/initialization?ref=main"
+locals {
   bounded_context_name = "ExampleContext"
+}
+
+module "serverless_bounded_context_initialization" {
+  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/initialization?ref=main"
+  bounded_context_name = local.bounded_context_name
 }
---
+++ iac/initialization/outputs.tf
@@ -0,0 +1,3 @@
+output "build_bucket_name" {
+  value = module.serverless_bounded_context_initialization.build_bucket_name
+}
