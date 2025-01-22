314b270f2f49328154e4d3ecde1403b69ce22574
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-22 01:22:36 -0700

    Use locals

--- iac/main.tf
+++ iac/main.tf
@@ -1,8 +1,14 @@
-module "example_lambda_context" {
-  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=main"
+locals {
   bounded_context_name = "ExampleContext"
   aggregate_root_name = "Example"
-  application_name = "example-handlers-${var.environment_name}"
+  application_name = "serverless-example"
+}
+
+module "serverless_bounded_context" {
+  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/bounded-context/aws/serverless/context?ref=main"
+  bounded_context_name = local.bounded_context_name
+  aggregate_root_name = local.aggregate_root_name
+  application_name = "${local.application_name}-${var.environment_name}"
   build_bucket_name = var.build_bucket_name
   handlers = var.handlers
 }
