2d69d542dd36fa2cdbc4c79ea323175a5790f591
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-22 22:45:56 -0700

    Fix module name

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,20 +1,20 @@
 output "event_bus_name" {
-  value = module.example_lambda_context.event_bus_name
+  value = module.serverless_bounded_context.event_bus_name
 }
 
 output "event_log_table_name" {
-  value = module.example_lambda_context.event_log_table_name
+  value = module.serverless_bounded_context.event_log_table_name
 }
 
 output "snapshot_table_name" {
-  value = module.example_lambda_context.snapshot_table_name
+  value = module.serverless_bounded_context.snapshot_table_name
 }
 
 output "lambda_uris" {
-  value = module.example_lambda_context.lambda_uris
+  value = module.serverless_bounded_context.lambda_uris
 }
 
 output "event_topic_arn" {
-  value = module.example_lambda_context.event_topic_arn
+  value = module.serverless_bounded_context.event_topic_arn
 }
 
