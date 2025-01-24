be13da96dd7ffd27a9506b599834d3828612a754
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-24 01:04:58 -0700

    Fix input

--- iac/vars.tf
+++ iac/vars.tf
@@ -10,6 +10,7 @@ variable "handlers" {
   type = list(object({
     name = string
     handles_commands = optional(bool, false)
+    materializes_views = optional(bool, false)
     event_sources = optional(list(string), [])
     build_id = string
   }))
