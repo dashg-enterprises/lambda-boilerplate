524dca2a1feb05ce552533bb1dceba0addb5f0ad
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-25 22:52:24 -0700

    Implement new abstract methods in Aggregate

--- src/command-handler/domain/Example.ts
+++ src/command-handler/domain/Example.ts
@@ -1,4 +1,4 @@
-import { Aggregate, EventLog, ISnapshot } from "@dashg-enterprises/ddd-platform";
+import { Aggregate, EventLog, IApplierMap, IHandlerMap, ISnapshot } from "@dashg-enterprises/ddd-platform";
 import { CreateExample } from "../commands/CreateExample";
 import { ExampleCreated, ExampleCreatedEvent } from "../events/ExampleCreated";
 import { ExampleUpdated } from "../events/ExampleUpdated";
@@ -6,22 +6,34 @@ import { ExampleStatus } from "../contracts/ExampleStatus";
 import { ExampleSnapshot } from "../contracts/ExampleSnapshot";
 
 export class Example extends Aggregate {
-    type = "Example";
-    toSnapshot(): ExampleSnapshot {
-        const { name, status, userId } = this;
-        return { name, status, userId }
-    }
-    private static initialStatus: ExampleStatus = ExampleStatus.Active;
+    public readonly type = "Example";
 
-    private status?: ExampleStatus;
-    private name?: string;
-    private userId?: string;
+    private static _initialStatus: ExampleStatus = ExampleStatus.Active;
+
+    private _status?: ExampleStatus;
+    private _name?: string;
+    private _userId?: string;
 
     constructor(eventLog?: EventLog) {
         super(eventLog);
-        this.registerHandler(this.create.bind(this)).for(CreateExample.metadata.type);
-        this.registerApplier(this.created.bind(this)).for(ExampleCreated.metadata.type);
     }
+
+    protected registerHandlers(): IHandlerMap {
+        let handlers = this
+            .withHandler(this.create.bind(this))
+            .for(CreateExample.metadata.type);
+
+        return handlers;
+    }
+
+    protected registerAppliers(): IApplierMap {
+        let appliers = this
+            .withApplier(this.created.bind(this))
+            .for(ExampleCreated.metadata.type);
+
+        return appliers;
+    }
+
     create(createExample: CreateExample) {
         if (!createExample.command.name || !createExample.command.userId) throw new Error("Name and userId are required for this example.");
 
@@ -29,25 +41,34 @@ export class Example extends Aggregate {
             this.id,
             createExample.command.userId,
             createExample.command.name,
-            Example.initialStatus
+            Example._initialStatus
         );
         const domainEvent = new ExampleCreated(exampleCreatedEvent, createExample);
         return domainEvent;
     }
 
     created(exampleCreated: ExampleCreated) {
-        this.name = exampleCreated.event.name;
-        this.userId = exampleCreated.event.userId;
-        this.status = ExampleStatus[exampleCreated.event.status as keyof typeof ExampleStatus];
+        this._name = exampleCreated.event.name;
+        this._userId = exampleCreated.event.userId;
+        this._status = ExampleStatus[exampleCreated.event.status as keyof typeof ExampleStatus];
     }
 
     getName() {
-        return this.name;
+        return this._name;
     }
     getStatus() {
-        return this.status;
+        return this._status;
     }
     getUserId() {
-        return this.userId;
+        return this._userId;
+    }
+
+    toSnapshot(): ExampleSnapshot {
+        const { _name, _status, _userId } = this;
+        return { 
+            name: _name,
+            status: _status,
+            userId: _userId
+        };
     }
 }
