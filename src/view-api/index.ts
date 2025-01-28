248b80af021680ff3d1205cf742443cbef2b07fc
chadgarlandscg <chadgarlandscg@gmail.com>
2025-01-28 02:44:47 -0700

    Controller fix

--- src/view-api/index.ts
+++ src/view-api/index.ts
@@ -1,23 +1,6 @@
 import 'reflect-metadata';
-import { APIGatewayEvent, Handler, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { host, hostApi } from '../view-materializer/inversify.config';
-import ExampleController from './presentation/ExampleController';
-import { TYPES } from '../view-materializer/TYPES';
-import { IExampleController } from './presentation/IExampleController';
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+import { hostApi } from '../view-materializer/inversify.config';
 
 /*global handler @preserve*/
 export const handler: Handler<APIGatewayEvent> = hostApi.withServerlessApi();
-// export const handler: Handler<APIGatewayEvent> = async (event, context): Promise<APIGatewayProxyResultV2<object>> => {
-//     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-//     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-//     const exampleController = host.get<IExampleController>(TYPES.IExampleController);
-
-//     switch (event.path) {
-//         case ExampleController.getPath:
-//             return await exampleController.get(event);
-//         case ExampleController.getByIdPath:
-//             return await exampleController.getById(event);
-//         default:
-//             return exampleController.pathNotFound(event.path);
-//     }
-// };
--- src/view-api/presentation/IExampleController.ts
+++
@@ -1,4 +0,0 @@
-import { IControllerBase } from "@dashg-enterprises/ddd-platform";
-
-export interface IExampleController extends IControllerBase {
-}
